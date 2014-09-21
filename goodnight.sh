ce137185bfbb828b214252ec136b8315912f858d
Mitchell Kember <mk12360@gmail.com>
2014-09-21 09:02:41 -0400

    Change date format of journal (flat is better than nested)

--- goodnight.sh
+++ goodnight.sh
@@ -2,10 +2,10 @@
 
 # This script moves the contents of Today.md to the end of Journal.md. (I use
 # Today.md because Journal.md is a huge file that will only get bigger, and I
-# don't want to have to scroll to the bottom or deal with slow loading.) If the
-# entry in Today.md is the first of the year or the month, those headings will
-# also be added. By default, the date used is the date five hours ago because I
-# write journal entries past midnight sometimes but never early in the morning.
+# don't want to have to scroll to the bottom or deal with slow loading.) The
+# entry is preceded by a heading of the form "# Weekday, Day Month Year". By
+# default, the date used is the date five hours ago because I write journal
+# entries past midnight sometimes but never early in the morning.
 
 name=$(basename "$0")
 usage="usage: $name [-hfd] [-t file] [-j file] [-n num] [-v[+|-]val[ymwdHMS]]"
@@ -50,15 +50,6 @@ else
 	[[ -s $today ]] || (echo "$name: $today: file is empty" >&2; exit 1)
 fi
 
-journal_last() {
-	if (( $tailn == 0 )); then
-		line=$(egrep "^#{$1} " $journal | tail -n 1)
-	else
-		line=$(tail -n $tailn $journal | egrep "^#{$1} " | tail -n 1)
-	fi
-	echo ${line:$1}
-}
-
 entry=$'\n\n'
 opts="-v $offset"
 year=$(date $opts +%Y)
@@ -66,22 +57,7 @@ month=$(date $opts +%B)
 weekday=$(date $opts +%A)
 day=$(date $opts +%e | xargs)
 
-if [[ $(journal_last 1) != $year ]]; then
-	entry+="# $year"$'\n\n'
-	entry+="## $month"$'\n\n'
-elif [[ $(journal_last 2) != $month ]]; then
-	entry+="## $month"$'\n\n'
-fi
-
-case $day in
-	*11|*12|*13) day+=th;;
-	*1) day+=st;;
-	*2) day+=nd;;
-	*3) day+=rd;;
-	 *) day+=th;;
-esac
-
-entry+="### $weekday the $day"$'\n\n'
+entry+="# $weekday, $day $month $year"$'\n\n'
 entry+=$(< $today)
 echo -n "$entry" >> $journal
 > $today
