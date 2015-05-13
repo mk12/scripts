6969704ee3d30e8399b1a7c9184244f74bc60d15
Mitchell Kember <mk12360@gmail.com>
2015-05-13 00:40:23 +0000

    Add script for setting up vagrant

---
+++ su-vagrant.sh
@@ -0,0 +1,16 @@
+# Use fish, not bash
+sudo apt-get install fish
+echo "\nfish" >> ~/.bashrc
+
+# Other packages
+sudo apt-get install silversearcher-ag
+
+# Add dotfiles
+mkdir $HOME/src/Development
+git clone git@github.com:mk12/dotfiles.git $HOME/src/Development/dotfiles
+rm $HOME/.gitconfig
+$HOME/src/Development/dotfiles/link.sh
+
+# Set up Vundle
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall now
