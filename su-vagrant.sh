3c33578ab9a06a5ff69c1e40bb1d1d0fe6677598
Mitchell Kember <mk12360@gmail.com>
2015-05-29 12:06:17 +0000

    Compile CtrP C matcher extension

--- su-vagrant.sh
+++ su-vagrant.sh
@@ -14,3 +14,7 @@ $HOME/src/Development/dotfiles/link.sh
 # Set up Vundle
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall now
+
+# Compile the CtrlP C matching extension
+cd ~/.vim/bundle/ctrlp-cmatcher
+./install.sh
