286ff692071302cb62a6f3a79d6b5e59ce4bf421
Mitchell Kember <mk12360@gmail.com>
2016-03-27 15:53:16 -0700

    Remove some more old scripts

--- pagerange.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import fileinput
-
-for line in fileinput.input():
-    date, pp = line.split(",", 1)
-    first, last = pp.split("-", 1)
-    print date + ",", int(last) - int(first) + 1
