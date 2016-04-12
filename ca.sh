86c38a29b74a4f2c612f35b28d31f25cf6b202c2
Mitchell Kember <mk12360@gmail.com>
2016-04-11 21:27:52 -0700

    Improve the exchange report in ca.sh

--- ca.sh
+++ ca.sh
@@ -85,6 +85,7 @@ invoke() {
 }
 
 summary() {
+	# Get the start of the year.
 	if [[ $end == "" ]]; then
 		jan1='this year'
 	else
@@ -112,6 +113,62 @@ summary() {
 	echo
 }
 
+exchange() {
+	echo "CURRENCY EXCHANGE"
+	echo "========================================"
+	invoke --invert bal '^Exchange' || exit 1
+	echo
+	# Get the end date.
+	if [[ $end == "" ]]; then
+		rate_date='latest'
+	else
+		rate_date=$(date -j -f '%Y/%m/%d' "$end" +'%Y-%m-%d' 2>/dev/null)
+		if [[ $rate_date == "" ]]; then
+			error "$end: exchange requires YYYY/MM/DD"
+		fi
+	fi
+	# Download and parse the CAD/USD exchange rate.
+	fixer="http://api.fixer.io/$rate_date?base=USD&symbols=CAD"
+	rate=$(curl -sS "$fixer" | grep -o '"CAD":[.0-9]\+' | cut -c 7-) || exit 1
+	# Invoke ledger with a simplified output format.
+	output=$(invoke --invert --format='%(T)' bal '^Exchange') || exit 1
+	# Extract CAD and USD from the Exchange account balance.
+	if [[ ${output:0:1} == '$' ]]; then
+		cad=$(echo "$output" | head -n 1 | cut -c 2-)
+		usd=$(echo "$output" | sed -n '2p' | cut -c 5-)
+		if [[ $usd == "" ]]; then
+			usd='0.00'
+		fi
+	elif [[ ${output:0:3} == 'USD' ]]; then
+		usd=$(echo "$output" | head -n 1 | cut -c 5-)
+		cad=$(echo "$output" | sed -n '2p' | cut -c 2-)
+		if [[ $cad == "" ]]; then
+			cad='0.00'
+		fi
+	else
+		# No exchange activity during this period.
+		exit 0
+	fi
+	# Remove any commas.
+	cad=${cad//,}
+	usd=${usd//,}
+	# Calculate the gain in terms of both currencies.
+	fmt='-\?[0-9]\+\.[0-9][0-9]'
+	cad_expr="$cad + ($usd * $rate)"
+	usd_expr="($cad / $rate) + $usd"
+	gain_cad=$(echo "$cad_expr" | bc -l | grep -o -- "$fmt") || exit 1
+	gain_usd=$(echo "$usd_expr" | bc -l | grep -o -- "$fmt") || exit 1
+	echo "NET EXCHANGE GAIN"
+	echo "========================================"
+	if [[ $rate_date == 'latest' ]]; then
+		echo "Using today's exchange rate:"
+	else
+		echo "Using the exchange rates of $(echo $rate_date | tr '-' '/'):"
+	fi
+	echo "\$$gain_cad (USD $gain_usd)"
+	echo
+}
+
 case ${1:0:1} in
 	a) invoke -E accounts ;;
 	b) invoke bal '^Assets' '^Liabilities' ;;
@@ -121,6 +178,6 @@ case ${1:0:1} in
 	n) invoke --invert --depth 2 bal '^Income' '^Expenses' ;;
 	p) invoke payees ;;
 	s) summary ;;
-	x) invoke --invert bal '^Exchange' ;;
+	x) exchange ;;
 	?) error ;;
 esac
