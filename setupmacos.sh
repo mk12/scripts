1024e5484c6194d494630c5f9e502788c1f902cc
Mitchell Kember <mk12360@gmail.com>
2018-11-15 23:08:10 -0800

    Update setupmacos.sh and make it idempotent

--- setupmacos.sh
+++ setupmacos.sh
@@ -1,106 +1,111 @@
 #!/bin/bash
 
-status() {
+set -ufo pipefail
+
+br=/usr/local/bin
+gh=~/GitHub
+
+step() {
 	echo -e "\x1b[31m[SETUP] $*...\x1b[0m"
 }
 
-br=/usr/local/bin
-gh=$HOME/GitHub
+brew_install() {
+	test -x $br/${2:-$1} || brew install $1
+}
 
-status 'Installing Xcode command line tools'
-xcode-select --install
+brew_cask_install() {
+	test -x "/Applications/$2.app" || brew install $1
+}
 
-status 'Installing Homebrew'
-/usr/bin/ruby -e "$(curl -fsSL \
+clone_git_repo() {
+	test -d $2 || $br/git clone git@github.com:$1 $2
+}
+
+step 'Installing Xcode command line tools'
+xcode-select --print-path > /dev/null || xcode-select --install
+
+step 'Installing Homebrew'
+test -x $br/brew || /usr/bin/ruby -e "$(curl -fsSL \
 	https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-status 'Installing Homebrew formulas'
-$br/brew install bash
-$br/brew install bash-completion@2
-$br/brew install git
-$br/brew install go
-$br/brew install keychain
-$br/brew install ledger
-$br/brew install neovim
-$br/brew install neovim/neovim/neovim
-$br/brew install python
-$br/brew install reattach-to-user-namespace
-$br/brew install ripgrep
-$br/brew install ruby
-$br/brew install tldr
-$br/brew install tmux
-$br/brew install trash
-$br/brew install vim
-$br/brew install zsh
-$br/brew install zsh-completion
+step 'Installing Homebrew formulas'
+brew_install bash
+brew_install fish
+brew_install git
+brew_install ledger
+brew_install neovim nvim
+brew_install python python3
+brew_install python@2 python2
+brew_install ripgrep rg
+brew_install tldr
+brew_install tmux
+brew_install trash
+brew_install vim
 
-status 'Installing Homebrew cask formulas'
-$br/brew cask install authy-desktop
-$br/brew cask install google-chrome
-$br/brew cask install lastpass
-$br/brew cask install visual-studio-code
+step 'Installing Homebrew cask formulas'
+brew_cask_install dropbox 'Dropbox'
+brew_cask_install google-chrome 'Google Chrome'
+brew_cask_install lastpass 'LastPass'
 
-status 'Installing Rust'
-curl 'https://sh.rustup.rs' -sSf | sh
+step 'Installing Rust'
+test -x ~/.cargo/bin/rustup || curl 'https://sh.rustup.rs' -sSf | sh
 
-status 'Installing Python packages'
+step 'Installing Python packages'
 $br/pip install neovim
-$br/pip3 install neovim pygments yapf
+$br/pip3 install neovim pygments
 
-status 'Installing Ruby gems'
-$br/gem install neovim pro
+step 'Setting up SSH'
+if ! [[ -f ~/.ssh/id_rsa ]]; then
+	step 'Generating an SSH key'
+	echo -n 'Enter email address to use with Git: '
+	read email_address
+	ssh-keygen -t rsa -b 4096 -C "$email_address"
 
-status 'Installing Go packages'
-$br/go get golang.org/x/tools/cmd/goimports
+	step 'Adding SSH key to ssh-agent'
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
 
-status 'Generating an SSH key'
-echo -n 'Enter email address to use with Git: '
-read email_address
-ssh-keygen -t rsa -b 4096 -C "$email_address"
+	step 'Adding SSH key to GitHub'
+	pbcopy < ~/.ssh/id_rsa.pub
+	echo 'Copied public SSH key to the clipboard'
+	echo -n 'Press return to open the browser to GitHub: '
+	read
+	open 'https://github.com/settings/keys'
+	echo -n 'Press return when ready: '
+	read
+fi
 
-status 'Adding SSH key to ssh-agent'
-eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_rsa
+step 'Creating GitHub directory'
+mkdir -p $gh
 
-status 'Adding SSH key to GitHub'
-pbcopy < $HOME/.ssh/id_rsa.pub
-echo 'Copied public SSH key to the clipboard'
-echo -n 'Press return to open the browser to GitHub: '
-read
-open 'https://github.com/settings/keys'
-echo -n 'Press return when ready: '
-read
+step 'Cloning repositories'
+clone_git_repo mk12/dotfiles $gh/dotfiles
+clone_git_repo mk12/finance $gh/finance
+clone_git_repo mk12/scripts $gh/scripts
 
-status 'Creating ~/GitHub directory'
-gh=$HOME/GitHub
-mkdir $gh
-
-status 'Cloning repositories'
-$br/git clone git@github.com:mk12/dotfiles $gh/dotfiles
-$br/git clone git@github.com:mk12/scripts $gh/scripts
-$br/git clone git@github.com:mk12/finance $gh/finance
-
-status 'Symlinking dotfiles'
+step 'Symlinking dotfiles'
 $gh/dotfiles/link.sh
 
-status 'Installing Tmux plugins'
-$br/git clone git@github.com:tmux-plugins/tpm ~/.tmux/plugins/tpm
+step 'Installing Tmux plugins'
+clone_git_repo tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-status 'Installing Vim plugins'
+step 'Installing Vim plugins'
 $br/vim +PlugInstall +qall
 $br/nvim +PlugInstall +qall
 
-status 'Symlinking ~/ia'
-ln -s "$HOME/Dropbox/iA Writer" $HOME/ia
-chflags -h hidden $HOME/ia
+step 'Symlinking ~/ia'
+if ! [[ -L ~/ia ]]; then
+	ln -s "$HOME/Dropbox/iA Writer" ~/ia
+	chflags -h hidden ~/ia
+fi
 
-status 'Changing login shell to fish'
-sudo chsh -s $br/fish $USER
+step 'Changing login shell to fish'
+dscl . -read ~ UserShell | grep -q $br/fish || sudo chsh -s $br/fish $USER
 
-status 'Finished'
+step 'Finished'
 echo 'Manual steps:'
-echo '  1. Set up iCloud and adjust System Preferences.'
-echo '  2. Install apps from the Mac App Store.'
+echo '  1. Set up iCloud.'
+echo '  2. Install apps from the App Store.'
 echo '  3. Install fonts from ~/Dropbox/fonts.'
-echo '  4. Configure Terminal profile settings.'
+echo '  4. Configure terminal profile settings.'
 echo '  5. Set HOMEBREW_GITHUB_API_TOKEN in ~/.config/fish/secret.fish.'
