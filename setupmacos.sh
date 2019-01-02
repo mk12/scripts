7ddf8539d477c77e47d37ecb590fc9b325cec1b4
Mitchell Kember <mk12360@gmail.com>
2019-01-01 23:06:44 -0500

    Greatly improved setupmacos.sh

--- setupmacos.sh
+++ setupmacos.sh
@@ -2,129 +2,203 @@
 
 set -ufo pipefail
 
+# Constants
 br=/usr/local/bin
 gh=~/GitHub
+fish_secret=~/.config/fish/secret.fish
+
+# Options
+primary=
+
+usage() {
+    cat <<EOS
+Setup macOS
+
+Usage: $(basename "$0") [options]
+
+This script configures a new Mac with all the settings and software I want. It's
+also idempotent, so running it twice is perfectly safe (and quick).
+
+Options:
+
+    -h  Print this help message
+    -p  Setup as the primary Mac. This includes some additional configuration
+        that should only be present on one machine at a time.
+EOS
+}
 
 step() {
     echo -e "\x1b[31m[SETUP] $*\x1b[0m"
 }
 
-brew_install() {
-    test -x $br/${2:-$1} || brew install $1
-}
-
-brew_cask_install() {
-    test -x "/Applications/$2.app" || brew cask install $1
-}
-
 clone_git_repo() {
-    test -d $2 || $br/git clone git@github.com:$1 $2
+    test -d "$2"|| "$br/git" clone "git@github.com:$1" "$2"
 }
 
-step 'Installing Xcode command line tools'
-xcode-select --print-path > /dev/null || xcode-select --install
+setup_xcode_cli() {
+    step "Installing Xcode command line tools"
+    xcode-select --print-path > /dev/null || xcode-select --install
+}
 
-step 'Installing Homebrew'
-test -x $br/brew || /usr/bin/ruby -e "$(curl -fsSL \
-    https://raw.githubusercontent.com/Homebrew/install/master/install)"
+setup_ssh_github() {
+    step "Setting up SSH"
+    if ! [[ -f ~/.ssh/id_rsa ]]; then
+        step "Generating an SSH key"
+        echo -n "Enter email address to use with Git: "
+        read -r email_address
+        ssh-keygen -t rsa -b 4096 -C "$email_address"
 
-step 'Installing Homebrew formulas'
-brew_install bash
-brew_install exa
-brew_install fish
-brew_install git
-brew_install ledger
-brew_install neovim nvim
-brew_install python python3
-brew_install python@2 python2
-brew_install ripgrep rg
-brew_install tldr
-brew_install tmux
-brew_install trash
-brew_install vim
+        step "Adding SSH key to keychain"
+        eval "$(ssh-agent -s)"
+        ssh-add -K ~/.ssh/id_rsa
 
-step 'Installing Homebrew cask formulas'
-brew_cask_install dropbox 'Dropbox'
-brew_cask_install google-chrome 'Google Chrome'
-brew_cask_install iterm2 'iTerm'
-brew_cask_install lastpass 'LastPass'
+        step "Adding SSH key to GitHub"
+        pbcopy < ~/.ssh/id_rsa.pub
+        echo "Copied public SSH key to the clipboard"
+        echo -n "Press return to open the browser to GitHub: "
+        read -r
+        open "https://github.com/settings/keys"
+        echo -n "Press return when ready: "
+        read -r
+    fi
+}
 
-step 'Installing Rust'
-test -x ~/.cargo/bin/rustup || curl 'https://sh.rustup.rs' -sSf | sh
+setup_my_repos() {
+    step "Creating GitHub directory"
+    mkdir -p "$gh"
 
-step 'Installing Python packages'
-$br/pip install neovim
-$br/pip3 install neovim pygments
+    step "Cloning repositories"
+    clone_git_repo mk12/dotfiles "$gh/dotfiles"
+    clone_git_repo mk12/finance "$gh/finance"
+    clone_git_repo mk12/scripts "$gh/scripts"
 
-step 'Setting up SSH'
-if ! [[ -f ~/.ssh/id_rsa ]]; then
-    step 'Generating an SSH key'
-    echo -n 'Enter email address to use with Git: '
-    read email_address
-    ssh-keygen -t rsa -b 4096 -C "$email_address"
+    step "Symlinking dotfiles"
+    "$gh/dotfiles/link.sh"
+}
 
-    step 'Adding SSH key to keychain'
-    eval "$(ssh-agent -s)"
-    ssh-add -K ~/.ssh/id_rsa
+setup_homebrew_github_api_token() {
+    step "Setting Homebrew GitHub API Token"
+    if ! grep -q HOMEBREW_GITHUB_API_TOKEN "$fish_secret"; then
+        echo "Create an access token called HOMEBREW_GITHUB_API_TOKEN for X"
+        echo -n "Press return to open the browser to GitHub: "
+        read -r
+        open "https://github.com/settings/tokens/new"
+        echo -n "Press return once you have copied the token: "
+        read -r
+        { echo -n 'set -x HOMEBREW_GITHUB_API_TOKEN "'; pbpaste; echo '"'; } \
+            >> "$fish_secret"
+    fi
+}
 
-    step 'Adding SSH key to GitHub'
-    pbcopy < ~/.ssh/id_rsa.pub
-    echo 'Copied public SSH key to the clipboard'
-    echo -n 'Press return to open the browser to GitHub: '
-    read
-    open 'https://github.com/settings/keys'
-    echo -n 'Press return when ready: '
-    read
-fi
+setup_simplenote_backup() {
+    clone_git_repo Simperium/simperium-python "$gh/simperium-python"
+    clone_git_repo hiroshi/simplenote-backup "$gh/simplenote-backup"
 
-step 'Adding Homebrew GitHub API Token'
-secret=~/.config/fish/secret.fish
-if ! grep -q HOMEBREW_GITHUB_API_TOKEN $secret; then
-    echo 'Create an access token called HOMEBREW_GITHUB_API_TOKEN for [mac]'
-    echo -n 'Press return to open the browser to GitHub: '
-    read
-    open 'https://github.com/settings/tokens/new'
-    echo -n 'Press return once you have copied the token: '
-    read
-    echo -n 'set -x HOMEBREW_GITHUB_API_TOKEN "' >> $secret
-    pbpaste >> $secret
-    echo '"' >> $secret
-fi
+    step "Setting Simplenote API Token"
+    if ! grep -q SIMPLENOTE_API_TOKEN "$fish_secret"; then
+        property="simperium_opts.token"
+        echo -n $property | pbcopy
+        echo "The string '$property' has been copied to the clipboard"
+        echo "Paste it into the web console in Simplenote and copy the result"
+        echo -n "Press return to open the browser to Simplenote: "
+        read -r
+        open "https://app.simplenote.com/"
+        echo -n "Press return once you have copied the token: "
+        read -r
+        { echo -n "set -x SIMPLENOTE_API_TOKEN ""; pbpaste; echo """; } \
+            >> "$fish_secret"
+    fi
 
-step 'Creating GitHub directory'
-mkdir -p $gh
+    "$gh/scripts/backup.sh" install
+}
 
-step 'Cloning repositories'
-clone_git_repo mk12/dotfiles $gh/dotfiles
-clone_git_repo mk12/finance $gh/finance
-clone_git_repo mk12/scripts $gh/scripts
+setup_homebrew() {
+    step "Installing Homebrew"
+    test -x "$br/brew" || /usr/bin/ruby -e "$(curl -fsSL \
+        https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-step 'Symlinking dotfiles'
-$gh/dotfiles/link.sh
+    step "Installing Homebrew formulas"
+    "$br/brew" bundle install --global --no-upgrade --verbose
+}
 
-step 'Linking iTerm2 preferences'
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder \
-    -string $gh/dotfiles/iterm2
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+setup_rust() {
+    step "Installing Rust"
+    test -x ~/.cargo/bin/rustup || curl "https://sh.rustup.rs" -sSf | sh
+}
 
-step 'Installing Tmux plugins'
-clone_git_repo tmux-plugins/tpm ~/.tmux/plugins/tpm
+setup_python() {
+    step "Installing Python packages"
+    "$br/pip" install neovim
+    "$br/pip3" install neovim pygments
+}
 
-step 'Installing Vim plugins'
-$br/vim +PlugInstall +qall
-$br/nvim +PlugInstall +qall
+setup_iterm2() {
+    step "Linking iTerm2 preferences"
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder \
+        -string "$gh/dotfiles/iterm2"
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder \
+        -bool true
+}
 
-step 'Symlinking ~/ia'
-if ! [[ -L ~/ia ]]; then
-    ln -s "$HOME/Dropbox/iA Writer" ~/ia
-    chflags -h hidden ~/ia
-fi
+setup_tmux() {
+    step "Installing Tmux plugins"
+    clone_git_repo tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
 
-step 'Changing login shell to fish'
-dscl . -read ~ UserShell | grep -q $br/fish || sudo chsh -s $br/fish $USER
+setup_vim() {
+    step "Installing Vim plugins"
+    "$br/vim" +PlugInstall +qall
+    "$br/nvim" +PlugInstall +qall
+}
 
-step 'Finished'
-echo 'Manual steps:'
-echo '  1. Configure System Preferences.'
-echo '  2. Install apps from the App Store.'
-echo '  3. Set up Time Machine backups.'
+setup_ia_symlink() {
+    step "Symlinking ~/ia"
+    if ! [[ -L ~/ia ]]; then
+        ln -s "$HOME/Dropbox/iA Writer" ~/ia
+        chflags -h hidden ~/ia
+    fi
+}
+
+setup_fish_login() {
+    step "Changing login shell to fish"
+    dscl . -read ~ UserShell | grep -q "$br/fish" \
+        || sudo chsh -s "$br/fish" "$USER"
+}
+
+main() {
+    setup_xcode_cli
+    setup_ssh_github
+    setup_my_repos
+    setup_homebrew_github_api_token
+    setup_homebrew
+
+    setup_ia_symlink
+    setup_iterm2
+    setup_python
+    setup_rust
+    setup_tmux
+    setup_vim
+
+    setup_fish_login
+
+    if [[ -n "$primary" ]]; then
+        setup_simplenote_backup
+    fi
+
+    step "Finished"
+    echo "Manual steps:"
+    echo "  1. Install apps from the App Store."
+    echo "  2. Configure System Preferences."
+    echo "  3. Set up Time Machine backups."
+}
+
+while getopts ":hp" opt; do
+    case $opt in
+        h) usage; exit 0 ;;
+        p) primary=1 ;;
+        \?) echo "illegal option: $OPTARG" >&2; exit 1 ;;
+        :) echo "missing argument: $OPTARG" >&2; exit 1 ;;
+    esac
+done
+
+main
