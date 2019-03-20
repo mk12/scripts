d04b0be4ab4d099b4aeb7f93538db2adfa46196a
Mitchell Kember <mk12360@gmail.com>
2019-03-20 18:09:47 -0400

    Improvements to itermcolormap.sh

--- itermcolormap.sh
+++ itermcolormap.sh
@@ -5,11 +5,19 @@ set -eufo pipefail
 prog=$(basename "$0")
 
 xdg_data_dir=${XDG_DATA_HOME:-$HOME/.local/share}/itermcolormap
-presets_file="$xdg_data_dir/presets.txt"
+all_presets_file="$xdg_data_dir/presets.txt"
+xdg_config_dir=${XDG_CONFIG_HOME:-$HOME/.config}/itermcolormap
+my_presets_file="$xdg_config_dir/default.txt"
+
+bin_plist="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+plist="$PROJECTS/dotfiles/iterm2/com.googlecode.iterm2.plist"
+start_marker="START_ITERMCOLORMAP"
+end_marker="END_ITERMCOLORMAP"
 
 update=false
 just_list=false
 map_alternates=false
+in_place=false
 
 say() {
     echo " * $*"
@@ -22,27 +30,31 @@ die() {
 
 usage() {
     cat <<EOF
-usage: $prog [-hula] [THEME ...]
+usage: $prog [-ahilu] [THEME ...]
 
 sets up color preset shortcuts for iTerm2
 
-This script accepts a list of color preset names as arguments, or allows you to
-interactively select them if none are provided. It produces XML to place in the
-<key>GlobalKeyMap</key> section of com.googlecode.iterm2.plist that makes the
-shortcuts Ctrl-Cmd-1, Ctrl-Cmd-2, etc. switch to those color presets.
+This script accepts a list of color preset names as arguments. It produces XML
+to place in the <key>GlobalKeyMap</key> section of com.googlecode.iterm2.plist
+that makes the shortcuts Ctrl-Cmd-1, Ctrl-Cmd-2, etc. switch to those color
+presets. If called without arguments, it looks for arguments in the config file.
+
+config file:
+    $my_presets_file
 
 options:
-    -h  show this help message
-    -u  regenerate the list of color preset names
-    -l  list the available presets
     -a  map alternate (light/dark) presets to Ctrl-Alt-Cmd-(number)
+    -h  show this help message
+    -i  edit the plist in place instead of dumping XML
+    -l  list the available presets
+    -u  regenerate the list of color preset names
 EOF
 }
 
 create_preset_cache() {
     mkdir -p "$xdg_data_dir"
-    if osascript <<EOF | sed '/^$/d' | sort > "$presets_file"
-set iTermPList to "~/Library/Preferences/com.googlecode.iterm2.plist"
+    if osascript <<EOF | sed '/^$/d' | sort > "$all_presets_file"
+set iTermPList to "$bin_plist"
 tell application "System Events"
     tell property list file iTermPList
         tell contents
@@ -97,14 +109,14 @@ write_mappings() {
                     "${name%-light}" \
                     "${name%-dark}" \
                     "${name%-night}" \
-                    "${name%dark}light" \
+                    "${name/dark/light}" \
                     "${name%-dark}light" \
                     "${name%dark}-light" \
-                    "${name%light}dark" \
+                    "${name/dark/light}" \
                     "${name%-light}dark" \
                     "${name%light}-dark"; do
                 if [[ "$name" != "$possibility" ]] && \
-                        grep -q "^$possibility$" "$presets_file"; then
+                        grep -q "^$possibility$" "$all_presets_file"; then
                     alternate=$possibility
                     break
                 fi
@@ -120,33 +132,57 @@ main() {
         create_preset_cache
         return
     fi
-
-    if ! [[ -f "$presets_file" ]]; then
+    if ! [[ -f "$all_presets_file" ]]; then
         die "preset cache does not exist (try running $prog -u)"
     fi
-
     if [[ "$just_list" == true ]]; then
-        cat "$presets_file"
+        cat "$all_presets_file"
         return
     fi
 
-    if [[ $# -gt 0 ]]; then
-        write_mappings "$@"
+    presets=("$@")
+    if [[ "${#presets[@]}" -eq 0 ]]; then
+        die "zero presets provided!"
+    fi
+    if [[ "${#presets[@]}" -gt 10 ]]; then
+        die "too many presets provided (max is 10)"
+    fi
+
+    if [[ "$in_place" == true ]]; then
+        if ! grep -q "$start_marker" "$plist"; then
+            die "$plist: does not contain '$start_marker'"
+        fi
+        if ! grep -q "$end_marker" "$plist"; then
+            die "$plist: does not contain '$end_marker'"
+        fi
+        out=$(mktemp)
+        sed -n -e "1,/$start_marker/p" < "$plist" > "$out"
+        write_mappings "${presets[@]+"${presets[@]}"}" >> "$out"
+        sed -n -e "/$end_marker/,\$p" < "$plist" >> "$out"
+        mv -f "$out" "$plist"
+        say "successfully edited $plist"
     else
-        presets=()
-        while read -r name; do
-            presets+=("$name")
-        done < <(fzf -m < "$presets_file")
         write_mappings "${presets[@]+"${presets[@]}"}"
     fi
 }
 
-while getopts "hula" opt; do
+if [[ $# -eq 0 ]]; then
+    say "looking for config in $my_presets_file"
+    if ! [[ -f "$my_presets_file" ]]; then
+        die "$my_presets_file: file not found"
+    fi
+    while read -r line; do
+        set -- "$@" "$line"
+    done < "$my_presets_file"
+fi
+
+while getopts "ahilu" opt; do
     case $opt in
-        h) usage; exit 0 ;;
-        u) update=true ;;
-        l) just_list=true ;;
         a) map_alternates=true ;;
+        h) usage; exit 0 ;;
+        i) in_place=true ;;
+        l) just_list=true ;;
+        u) update=true ;;
         *) exit 1 ;;
     esac
 done
