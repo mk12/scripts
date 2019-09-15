ac1fdd3b6902491b59b2417461a5189de0522ef0
Mitchell Kember <mk12360@gmail.com>
2019-09-15 13:54:20 -0700

    Improve ledgerlint script

--- ledgerlint.py
+++ ledgerlint.py
@@ -8,7 +8,23 @@ import sys
 DIGITS = '0123456789'
 DATE_FMT = '%Y/%m/%d'
 
-DECIMAL = re.compile('[0-9]\.[0-9]')
+DECIMAL = re.compile(r'[0-9]\.[0-9]')
+
+COMMODITIES = [
+    "USD", "EUR", "VMFXX", "VTSAX", "VTIAX", "VBTLX", "VTRTS"
+]
+
+DECIMAL_PLACES = {
+    "$": 2,
+    "USD": 2,
+    "EUR": 2,
+    "VMFXX": 2,
+    "VTSAX": 4,
+    "VTIAX": 4,
+    "VBTLX": 4,
+    "VTRTS": 4,
+}
+
 
 def error(msg):
     print("{}:{}: {}".format(
@@ -17,31 +33,50 @@ def error(msg):
         msg),
         file=sys.stderr)
 
+
 def check_amount(s, endcol):
     if '  $' in s:
+        commodity = "$"
         i = s.rindex('  $')
         if not s[i+3] in DIGITS + '-':
             error("dollar sign not next to amount")
         amt = s[i+3:endcol]
-    elif '  USD' in s:
-        i = s.rindex('  USD')
-        if s[i+5] != ' ':
-            error("missing space after USD")
-        amt = s[i+6:endcol]
-    elif '  EUR' in s:
-        i = s.rindex('  EUR')
-        if s[i+5] != ' ':
-            error("missing space after EUR")
-        amt = s[i+6:endcol]
     else:
-        error("expected $ or USD or EUR")
+        for c in COMMODITIES:
+            needle = f"  {c}"
+            if needle in s:
+                commodity = c
+                i = s.rindex(needle)
+                if s[i+len(needle)] != ' ':
+                    error(f"missing space after {c}")
+                amt = s[i+len(needle)+1:endcol]
+                break
+        else:
+            cs = ", ".join(COMMODITIES)
+            error(f"expected one of the following commodities: $, {cs}")
+            return
+    p = DECIMAL_PLACES[commodity]
+    if len(amt) < p + 1 or amt[-p-1] != ".":
+        if "." in amt:
+            got = len(amt) - amt.rindex(".") - 1
+            error(f"expected {p} decimal places, only got {got}")
+            return
+        error(f"expected {p} decimal places, got none")
         return
-    whole = amt[:-3]
+    whole = amt[:-p-1]
     if whole[0] == '-':
         whole = whole[1:]
     commas = [c for i, c in enumerate(whole) if (len(whole) - i) % 4 == 0]
     if commas != [','] * len(commas):
         error("expected commas to separate thousands")
+
+
+def get_desired_decimal_places(line):
+    for commodity, places in DECIMAL_PLACES.items():
+        if commodity in line:
+            return places
+    return 2
+
 
 mode = 0
 note = ""
@@ -105,30 +140,50 @@ for line in fileinput.input():
             mode = 1
             asserted = False
             continue
+        p = get_desired_decimal_places(s[:60])
         if DECIMAL.match(s[:60]) or (len(s) >= 60 and s[59] in DIGITS):
             if len(s) < 58:
                 error("misaligned #.# (assumed to be price)")
-            if s[57] != '.':
+            if s[60-p-1] != '.':
                 error("value has misaligned '.'")
-            if not s[58] in DIGITS:
-                error("expected cent digit")
-            if not s[59] in DIGITS:
-                error("expected cent digit")
+            for i in range(p):
+                if not s[60-i-1] in DIGITS:
+                    error("expected decimal digit")
             check_amount(s[:60], 60)
+        col = 60
+        if '{' in s:
+            # lot prices, too complicated to check for now
+            col += 20
+        if '@' in s:
+            if len(s) < col + 20:
+                error("commodity cost is not flush right")
+                continue
+            if s[col+1] != '@':
+                error("commodity cost has misaligned '@")
+            p = get_desired_decimal_places(s[col:col+20])
+            if s[col+20-p-1] != '.':
+                error("commodity cost has misaligned '.'")
+            for i in range(p):
+                if not s[col+20-i-1] in DIGITS:
+                    error("expected decimal digit")
+            check_amount(s[col-18:col+20], col + 20)
+            col += 20
         if '=' in s:
             asserted = True
-            if len(s) < 80:
+            if len(s) > col + 20 and not s[col:col+20].strip():
+                col += 20
+            if len(s) < col + 20:
                 error("balance assertion is not flush right")
                 continue
-            if s[61] != '=':
+            if s[col+1] != '=':
                 error("balance assertion has misaligned '='")
-            if s[77] != '.':
+            p = get_desired_decimal_places(s[col:col+20])
+            if s[col+20-p-1] != '.':
                 error("balance assertion has misaligned '.'")
-            if not s[78] in DIGITS:
-                error("expected cent digit in balance assertion")
-            if not s[79] in DIGITS:
-                error("expected cent digit in balance assertion")
-            check_amount(s[62:80], 80)
+            for i in range(p):
+                if not s[col+20-i-1] in DIGITS:
+                    error("expected decimal digit")
+            check_amount(s[col+2:col+20], col + 20)
         elif not asserted and not 'Assets:Cash' in s:
             if ('ATM' in payee or 'Transfer' in payee or 'Pay debt' in note or
                     'Collect debt' in note or 'Visa statement' in note or
