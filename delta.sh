d4a5d8fb4dbad5ccc4f5f355fe1cc4abf6e311e1
Mitchell Kember <mk12360@gmail.com>
2020-06-01 01:16:53 -0400

    Adjustments to delta.sh

--- delta.sh
+++ delta.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 exec delta \
+    --24-bit-color never \
+    --hunk-header-style 'normal' \
     --zero-style 'normal' \
     --minus-style 'red' --minus-emph-style 'black red' \
     --plus-style 'green' --plus-emph-style 'black green' \
     --file-style 'bold blue' \
-    --file-decoration-style 'bold blue underline'
+    --file-decoration-style 'bold blue underline' \
+    "$@"
