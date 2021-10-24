2f1934159a55c082e733a6788e2090ba96bb5d25
Mitchell Kember <mk12360@gmail.com>
2021-10-24 15:52:20 -0700

    Add script for switching branch name to main

---
+++ git-master-to-main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+die() {
+    echo "$*" >&2
+    exit 1
+}
+
+step() {
+    echo "> $*"
+    "$@"
+}
+
+[[ -d .git ]] || die "not in a git repo"
+
+echo -n "Copy token from https://github.com/settings/tokens/new with repo permissions: "
+read -r token
+[[ -n "$token" ]] || die "token is empty"
+
+user=mk12
+repo=$(basename "$(pwd)")
+line="origin"$'\t'"git@github.com:$user/$repo.git"
+both_lines="$line (fetch)"$'\n'"$line (push)"
+[[ "$(git remote -v)" == "$both_lines" ]] || die "unexpected remote"
+
+step git branch -m master main
+step git push -u origin main
+step git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
+step curl --fail -X PATCH \
+    -u "$user:$token" \
+    -H 'Content-Type: application/json' \
+    -H "Accept: application/vnd.github.v3+json" \
+    -d "{\"name\": \"$repo\", \"default_branch\": \"main\"}" \
+    "https://api.github.com/repos/$user/$repo"
+step git push origin --delete master
