63a04534b840d968a38fe284a9beef97da17c0aa
Mitchell Kember <mk12360@gmail.com>
2022-05-28 22:02:35 -0700

    Update yank for kitty changes
    Kitty no longer uses a special chunking protocol:
    https://github.com/kovidgoyal/kitty/commit/8f214c51c092d79e90fea25ec2e5db117af0054b
    
    Now it just lets you put a huge payload in the OSC 52 escape. So changed
    yank.cpp to just do that. I hate this C++ code but too lazy to rewrite
    it all now. Tested inside and outside tmux.

--- yank.cpp
+++ yank.cpp
@@ -1,9 +1,7 @@
-#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <memory>
-#include <thread>
 
 extern "C" {
 #include <unistd.h>
@@ -12,12 +10,11 @@ extern "C" {
 namespace {
 
 constexpr const char* USAGE =
-    R"EOS(usage: yank [-h]
+    R"EOS(usage: yank [-ht]
 
-Copies standard input to the clipboard using OSC 52 escape sequences. Assumes
-the terminal implements the modified protocol whereby an invalid sequence clears
-the clipboard and a valid one appends to it (i.e. what kitty does). This allows
-the program to function properly for any amount of input.
+Copies standard input to the clipboard using the OSC 52 escape sequence. Assumes
+the terminal supports an unbounded payload like kitty does, so does not attempt
+to truncate it. Pass the -t flag to truncate to 8192 bytes.
 
 If $TMUX is set, yank also copies the input to the current tmux buffer. For best
 results, the tmux clipboard feature should be turned OFF:
@@ -72,8 +69,6 @@ constexpr std::size_t NON_TMUX_ESCAPE_SIZE =
 const char* prefix = "";
 const char* suffix = "";
 
-void reset_osc_52() { std::printf("%s\x1b]52;c;!\a%s", prefix, suffix); }
-
 void print_osc_52(const char* const base64, const std::size_t len) {
     std::printf("%s\x1b]52;c;%.*s\a%s", prefix, static_cast<int>(len), base64,
                 suffix);
@@ -84,44 +79,66 @@ void print_osc_52(const char* const base64, const std::size_t len) {
 int main(int argc, char** argv) {
     (void)argv;
     std::ios::sync_with_stdio(false);
+    bool truncate = false;
     if (argc > 1) {
-        std::fputs(USAGE, stdout);
-        return 0;
+        if (argc == 2 && std::strcmp(argv[1], "-h") == 0) {
+            std::fputs(USAGE, stdout);
+            return 0;
+        }
+        if (argc == 2 && std::strcmp(argv[1], "-t") == 0) {
+            truncate = true;
+        } else {
+            std::fputs(USAGE, stderr);
+            return 1;
+        }
     }
     const bool tmux = std::getenv("TMUX") != nullptr;
     std::unique_ptr<FILE, decltype(&pclose)> tmux_pipe(nullptr, pclose);
     if (tmux) {
-        // Wrap each OSC 52 sequence in a tmux passthrough envelope.
+        // Wrap the OSC 52 sequence in a tmux passthrough envelope.
         prefix = "\x1bPtmux;\x1b";
         suffix = "\x1b\\";
         // Run `tmux load-buffer -` in writable mode.
         tmux_pipe.reset(popen("tmux load-buffer -", "w"));
     }
-    // There is clearly a limit of 8192 somewhere, because this program breaks
-    // if I make it any larger (it starts printing stuff to the screen).
-    constexpr auto B64_BUF_SIZE = 8192 - NON_TMUX_ESCAPE_SIZE;
-    constexpr auto BUF_SIZE = base64_dec_size(B64_BUF_SIZE);
-    auto b64_buf = std::make_unique<char[]>(base64_enc_size(BUF_SIZE));
-    auto buf = std::make_unique<char[]>(BUF_SIZE);
-    reset_osc_52();
-    while (std::cin.good()) {
-        std::cin.read(buf.get(), BUF_SIZE);
-        const auto len = static_cast<std::size_t>(std::cin.gcount());
-        if (len == 0) {
-            break;
-        }
-        const auto b64_len = base64_enc_size(len);
-        base64_encode(b64_buf.get(), buf.get(), len);
-        if (tmux) {
-            // Sleep 1ms between OSC 52 writes for tmux. Otherwise it has flaky
-            // behavior, randomly dropping passthrough envelopes.
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            const auto n = std::fwrite(buf.get(), 1, len, tmux_pipe.get());
-            if (n != len) {
-                std::fprintf(stderr, "ERROR: %zu, want %zu\n", n, len);
+    char* buf = nullptr;
+    std::size_t len = 0;
+    std::size_t cap;
+    if (truncate) {
+        cap = base64_dec_size(8192 - (tmux ? 0 : NON_TMUX_ESCAPE_SIZE));
+        buf = static_cast<char*>(std::malloc(cap));
+        std::cin.read(buf, cap);
+        len = static_cast<std::size_t>(std::cin.gcount());
+    } else {
+        const auto chunk_size = 16384;
+        cap = chunk_size;
+        buf = static_cast<char*>(std::malloc(cap));
+        while (std::cin.good()) {
+            if (len + chunk_size > cap) {
+                while (len + chunk_size > cap) {
+                    cap *= 2;
+                }
+                buf = static_cast<char*>(std::realloc(buf, cap));
             }
+            std::cin.read(buf + len, chunk_size);
+            const auto additional = static_cast<std::size_t>(std::cin.gcount());
+            if (additional == 0) {
+                break;
+            }
+            len += additional;
         }
-        print_osc_52(b64_buf.get(), b64_len);
     }
+    const auto b64_len = base64_enc_size(len);
+    char *b64_buf = static_cast<char*>(std::malloc(b64_len));
+    base64_encode(b64_buf, buf, len);
+    if (tmux) {
+        const auto n = std::fwrite(buf, 1, len, tmux_pipe.get());
+        if (n != len) {
+            std::fprintf(stderr, "ERROR: %zu, want %zu\n", n, len);
+        }
+    }
+    print_osc_52(b64_buf, b64_len);
+    std::free(buf);
+    std::free(b64_buf);
     return 0;
 }
