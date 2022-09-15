012ad4e51be7a9ec5369899cb0f9d05b20745afa
Mitchell Kember <mk12360@gmail.com>
2022-09-15 11:35:18 -0700

    Just use xterm-kitty and tmux-256color
    I was going to do tmux-direct and RGB instead of tmux-256color and Tc.
    It seemed to work but fish's brblack was wrong, emitting RGB even though
    I disabled fish_term24bit. Turns out direct/RGB is a bad idea:
    
    https://github.com/kovidgoyal/kitty/issues/1141
    https://github.com/kovidgoyal/kitty/commit/18fe2e8dfa34038aabd5c3a2fdb3624e2b27932a
    https://github.com/kovidgoyal/kitty/commit/a91b3b9487ea9b508a2a446b6b0339e9aaba533f

--- install-terminfo.sh
+++ install-terminfo.sh
@@ -2,62 +2,90 @@
 
 set -eufo pipefail
 
+supported=(
+    xterm-kitty
+    tmux-256color
+)
+
 usage() {
     cat <<EOS
-Usage: $0
+Usage: $0 [NAME]
 
-Install some terminfo:
+Install terminfo entries
 
-- xterm-kitty
-- tmux-256color
-- tmux-direct
+Arguments:
+    NAME  The terminfo to install (if omitted, installs all)
+
+Supported terminfos:
+    ${supported[*]}
 EOS
 }
-
-if [[ $# -ne 0 ]]; then
-    usage
-    exit 1
-fi
 
 tmp=$(mktemp)
 trap 'rm "$tmp"' EXIT
 
-has() {
+die() {
+    echo >&2 "$0: $*"
+    exit 1
+}
+
+ensure() {
+    if [[ $# -gt 1 ]]; then
+        for name; do
+            ensure "$name"
+        done
+        return
+    fi
+
     # Clear TERMINFO to avoid a false positive for xterm-kitty when running
     # within kitty (we still want to install it in ~/.terminfo, otherwise it
     # doesn't work when ssh'ing to localhost).
-    TERMINFO="" infocmp "$1" &> /dev/null || return 1
-}
+    if TERMINFO="" infocmp "$1" &> /dev/null; then
+        echo "$1 is already installed"
+        return
+    fi
 
-if ! has xterm-kitty; then
-    if [[ -z ${KITTY_PID+x} ]]; then
-        cat >&2 <<EOS
-Cannot install xterm-kitty because we are not (directly) in kitty.
-If this is a remote machine, run this locally to install xterm-kitty here:
+    case $1 in
+        xterm-kitty)
+            if [[ -z ${KITTY_PID+x} ]]; then
+                cat >&2 <<EOS
+Cannot install $1 because we are not (directly) in kitty.
+If this is a remote machine, run this locally to install $1 on it:
 
     kitty +kitten ssh $(whoami)@$(hostname)
 EOS
-    else
-        infocmp -a xterm-kitty > "$tmp"
-        tic -x -o ~/.terminfo "$tmp"
-        echo "Installed xterm-kitty"
-    fi
-fi
+                return
+            fi
+            infocmp -a "$1" > "$tmp"
+            tic -x -o ~/.terminfo "$tmp"
+            ;;
 
-tmux_terminfo() {
-    has "$1" && return
-    case $(uname -s) in
-        Darwin) ;;
-        *) echo >&2 "I only know how to install $1 on macOS"; return ;;
+        tmux-256color)
+            case $(uname -s) in
+                Darwin) ;;
+                *) echo >&2 "I only know how to install $1 on macOS"; return ;;
+            esac
+            prefix=$(brew --prefix ncurses)
+            if ! [[ -d "$prefix" ]]; then
+                brew install ncurses
+            fi
+            "$prefix/bin/infocmp" -A "$prefix/share/terminfo" "$1" > "$tmp"
+            tic -o ~/.terminfo "$tmp"
+            ;;
+
+        *) die "$1: unsupported terminfo"
     esac
-    prefix=$(brew --prefix ncurses)
-    if ! [[ -d "$prefix" ]]; then
-        brew install ncurses
-    fi
-    "$prefix/bin/infocmp" -A "$prefix/share/terminfo" "$1" > "$tmp"
-    tic -o ~/.terminfo "$tmp"
     echo "Installed $1"
 }
 
-tmux_terminfo tmux-256color
-tmux_terminfo tmux-direct
+case $# in
+    0)
+        ensure "${supported[@]}"
+        ;;
+    *)
+        case $1 in
+            -h|--help|help) usage ;;
+            *) ensure "$@" ;;
+        esac
+        ;;
+esac
