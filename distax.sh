9e68c77123298a0d01cd54b2653b28ddbeb47b32
Mitchell Kember <mk12360@gmail.com>
2022-11-19 19:40:12 -0800

    Add script for distributing tax in ledger

---
+++ distax.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+input=$(pbpaste)
+
+gawk '
+match($0, /^(.* USD )([0-9]+\.[0-9]+)$/, m) {
+    if (NR == FNR) {
+        items[i++] = m[2];
+        subtotal += m[2];
+    } else {
+        printf "%s%.2f\n", m[1], items[i++];
+    }
+    next;
+}
+match($0, / USD -([0-9]+\.[0-9]+)$/, m) {
+    if (NR == FNR) {
+        total = m[1];
+        for (j = 0; j < i; j++) {
+            items[j] = items[j] / subtotal * total;
+        }
+        i = 0;
+    } else {
+        print;
+    }
+    next;
+}
+NR != FNR {
+    print;
+}
+END {
+    printf "Distributed %.2f\n", total - subtotal > "/dev/stderr";
+}
+' <(echo "$input") <(echo "$input") | yank
+
