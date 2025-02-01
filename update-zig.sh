d9c4a1c43dc6e65455bccc6d241583a812a252ff
Mitchell Kember <mk12360@gmail.com>
2025-02-01 15:39:09 -0500

    Update update-zig.sh

--- update-zig.sh
+++ update-zig.sh
@@ -12,28 +12,34 @@ run() {
     "$@"
 }
 
-if [[ ${1-} == "" || ${1-} == zig ]]; then
-    run cd ~/Documents/Zig
+install() {
+    name=$1
+    key=${2#key=}
+    dir=${3#dir=}
+    index_url=$4
+    run cd ~/Documents/"$name"
     fd -q -d 1 -e tar -e xz && die "There is already a .tar or .xz file"
-    url=$(run curl -sL https://ziglang.org/download/index.json \
-        | run jq -r '.master."aarch64-macos".tarball')
-    name=${url##*/}
-    name=${name%.tar.xz}
-    [[ -z "$name" ]] && die "Failed to extract name"
+    read -r url shasum < <(
+        run curl -sL "$index_url" \
+        | run jq -r $key'."aarch64-macos" | "\(.tarball) \(.shasum)"')
+    id=${url##*/}
+    id=${id%.tar.xz}
+    [[ -z "$id" ]] && die "Failed to extract id"
+    [[ -n "$dir" ]] && mkdir "$id"
     run curl -LO "$url"
-    run xz -d "$name.tar.xz"
-    run tar xf "$name.tar"
-    run rm "$name.tar"
-    run sim i -f "$name/zig"
+    run shasum --check -a 256 <<< "$shasum  $id.tar.xz"
+    run xz -d "$id.tar.xz"
+    run tar ${dir:+-C "$id"} -xf "$id.tar"
+    run rm "$id.tar"
+    run sim i -f "$id/$name"
+}
+
+if [[ ${1-zig} == zig ]]; then
+    install zig key=.master dir= https://ziglang.org/download/index.json
 fi
 
-
-if [[ ${1-} == "" || ${1-} == zls ]]; then
-    run cd "$(sim path)"
-    run rm zls
-    version=$(run curl -sL https://zigtools-releases.nyc3.digitaloceanspaces.com/zls/index.json \
-        | run jq -r '.latest')
-    run curl -LO "https://zigtools-releases.nyc3.digitaloceanspaces.com/zls/$version/aarch64-macos/zls"
-    run chmod +x zls
+if [[ ${1-zls} == zls ]]; then
+    zig_version=$(run zig version)
+    zig_version=${zig_version//+/%2B}
+    install zls key= dir=yes "https://releases.zigtools.org/v1/zls/select-version?zig_version=$zig_version&compatibility=only-runtime"
 fi
-
