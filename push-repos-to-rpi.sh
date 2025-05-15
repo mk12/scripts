ceb1986824523b8e756a844324c385e94fceecc0
Mitchell Kember <mk12360@gmail.com>
2025-05-14 20:30:47 -0400

    More work on pushing repos to rpi

--- push-repos-to-rpi.sh
+++ push-repos-to-rpi.sh
@@ -21,12 +21,26 @@ add_all_remote() {
     run git config --add remote.all.pushurl $(git remote get-url rpi)
 }
 
+remove_all_remote() {
+    git remote -v | grep -q '^all' || return
+    git remote rm all
+    git config --unset remote.pushDefault
+}
+
 push_main() {
     git remote -v | grep -q '^all' || return
     [[ -z "$(git diff)" ]] || return
     git rev-parse --verify main || return
     git config remote.pushDefault all
     git push all main
+}
+
+for_mine() {
+    :
+}
+
+for_others() {
+    :
 }
 
 email=$(git config user.email)
@@ -36,10 +50,11 @@ for dir in ~/Code/*; do
     fi
     run cd "$dir"
     author=$(git log --format='%ae' $(git rev-list --max-parents=0 HEAD | head -n1))
-    if [[ "$author" != "$email" ]]; then
-        continue
+    if [[ "$author" = "$email" ]]; then
+        for_mine "$dir" &
+    else
+        for_others "$dir" &
     fi
-    push_main "$dir" &
     sleep 0.1
 done
 wait
